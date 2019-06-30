f357061fcf069e5dead1d3bf26b110717ba578e6
1209simran <1209simran@gmail.com>
2019-06-30 23:21:49 +0530

    first commit

---
+++ src/Search/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Input, Card, CardBody, CardTitle } from "mdbreact";
+
+import "./style.css";
+
+import newsList from "./news.json";
+
+class App extends Component {
+  state = {
+    search: ""
+  };
+
+  renderNews = news => {
+    const { search } = this.state;
+
+    return (
+      <div className="col-md-3" style={{ marginTop: "20px" }}>
+        <Card>
+          <CardBody>
+            <CardTitle title={news.name}>
+              {news.name.substring(0, 15)}
+              {news.name.length > 15 && "..."}
+            </CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  };
+
+  onchange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  render() {
+    const { search } = this.state;
+    const filteredNews = newsList.filter(news => {
+      return news.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+
+    return (
+      <div className="flyout">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <center>
+                <h1
+                  style={{
+                    marginTop: "30px",
+                    color: "rgb(174, 238, 235)",
+                    fontFamily: "Droid Serif",
+                    fontWeight: "200"
+                  }}
+                >
+                  List Of Channels
+                </h1>
+              </center>
+            </div>
+            <div className="col search">
+              <Input label="Search" icon="search" onChange={this.onchange} />
+            </div>
+            <div className="col" />
+          </div>
+          <div className="row">
+            {filteredNews.map(news => {
+              return this.renderNews(news);
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
